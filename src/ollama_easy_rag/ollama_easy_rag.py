14f477942cbdb1362c03185404fd581ed32edaff
Jayant Malik <dev.jayantmalik@gmail.com>
2025-02-19 11:07:33 +0530

    removed deps on semantic-text-splitter and added support for source and chunk_id

--- src/ollama_easy_rag/ollama_easy_rag.py
+++ src/ollama_easy_rag/ollama_easy_rag.py
@@ -1,11 +1,10 @@
 import dataclasses
-from typing import List, Sequence, Iterator, Callable, Union
+from typing import List, Sequence, Iterator, Callable, Union, Optional
 
 import lancedb
 import ollama
 import pyarrow as pa
 from ollama import EmbedResponse, ChatResponse
-from semantic_text_splitter import TextSplitter
 
 
 @dataclasses.dataclass
@@ -18,12 +17,35 @@ class ModelPrompt:
 
 
 @dataclasses.dataclass
+class SearchSource:
+    source: str
+    chunk_id: str
+
+    def serialise(self):
+        return {"source": self.source, "chunk_id": self.chunk_id}
+
+
+@dataclasses.dataclass
+class SearchResult:
+    prediction: str
+    sources: List[SearchSource]
+
+    def serialise(self):
+        return {"prediction": self.prediction, "sources": [source.serialise() for source in self.sources]}
+
+
+@dataclasses.dataclass
 class TableContent:
     content: str
     vectors: List[float]
+    source: Optional[str]
+    chunk_id: Optional[str]
 
     def serialise(self) -> dict:
-        return {"vectors": self.vectors, "content": self.content}
+        return {"vectors": self.vectors,
+                "content": self.content,
+                "source": self.source if self.source is not None else "",
+                "chunk_id": self.chunk_id if self.chunk_id is not None else ""}
 
 
 class OllamaEasyRag:
@@ -61,33 +83,29 @@ class OllamaEasyRag:
         schema = pa.schema(
             [
                 pa.field("vectors", pa.list_(pa.float32(), list_size=self.vector_cols_count)),
-                pa.field("content", pa.string())
+                pa.field("content", pa.string()),
+                pa.field("source", pa.string(), nullable=True),
+                pa.field("chunk_id", pa.string(), nullable=True)
             ])
         self.db.create_table(self.table_name, schema=schema, exist_ok=True)
 
-    def insert_data(self, content: str, skip_duplicates=True, chunk_size: int = 1000,
-                    splitter: Callable[[str], List[str]] = None) -> None:
+    def insert_data(self, data: List[TableContent], skip_duplicates=True) -> None:
         """
         Inserts provided data in the database.
-        :param splitter: (content) -> List[str] a function that splits long text content in multiple small chunks
+        :param create_chunks: a function that splits long text content in multiple small chunks
         :param chunk_size: Our default text splitter uses chunk_size to split text. max length of text chunk = chunk_size
         :param skip_duplicates: Should we ignore insert incase
-        :param content: Text content to insert in database
+        :param data: content to insert in database
         :return: None
         """
 
-        # split text in multiple chunks
-        default_splitter = TextSplitter(capacity=chunk_size)
-        chunks = default_splitter.chunks(content) if splitter is None else splitter(content)
-
-        # compute vectors for each chunk and open table
-        items = [TableContent(content=chunk, vectors=self.compute_vectors(chunk)).serialise() for chunk in chunks]
+        # open table
         tbl = self.db.open_table(self.table_name)
 
         if skip_duplicates:
-            tbl.merge_insert("content").when_matched_update_all().when_not_matched_insert_all().execute(items)
+            tbl.merge_insert("content").when_matched_update_all().when_not_matched_insert_all().execute(data)
         else:
-            tbl.add(items)
+            tbl.add(data)
 
     def compute_vectors(self, content: str) -> Union[List[float], Sequence[float]]:
         """
@@ -122,7 +140,7 @@ class OllamaEasyRag:
             for chunk in response:
                 yield chunk.message.content
 
-    def search(self, query: str, stream: bool = False) -> Union[str, Iterator[str]]:
+    def search(self, query: str, stream: bool = False) -> SearchResult:
         """
         1. Perform vector search based for query
         2. Generates the output via AI model using results from step 1 as context.
@@ -135,9 +153,24 @@ class OllamaEasyRag:
         # compute vectors and search in database
         query_vectors = self.compute_vectors(query)
         table = self.db.open_table(self.table_name)
-        context = table.search(query_vectors).limit(5).select(["content"]).to_list()
+        context = table.search(query_vectors).limit(5).select(["content", "source", "chunk_id"]).to_list()
         context = "\n".join([record["content"] for record in context])
 
         # Generate model output based on context
         result = self.complete(prompts=self.create_prompts(context, query), stream=stream)
-        return result if stream else list(result)[0]
+
+        # Return search results
+        sources = []
+        for record in context:
+            source = record["source"]
+            chunk_id = record["chunk_id"]
+            if source is None and chunk_id is None:
+                continue
+
+            sources.append(SearchSource(source=source if source is not None else "",
+                                        chunk_id=chunk_id if chunk_id is not None else ""))
+
+        if stream:
+            return SearchResult(prediction=result, sources=sources)
+        else:
+            return SearchResult(prediction=list(result)[0], sources=sources)
